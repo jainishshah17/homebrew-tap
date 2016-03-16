a5723622e54507e8ea790617cf7a0479837ab9e1
Jainish Shah <Jainishshah@yahoo.com>
2016-03-16 12:51:48 -0700

    Update jfrog-cli-go.rb

--- jfrog-cli-go.rb
+++ jfrog-cli-go.rb
@@ -1,5 +1,5 @@
 class JfrogCliGo < Formula
-  desc "A command-line interface for Jfrog Artifactory and Bintray"
+  desc "command-line interface for Jfrog Artifactory and Bintray"
   homepage "https://github.com/JFrogDev/jfrog-cli-go"
   url "https://github.com/JFrogDev/jfrog-cli-go/archive/1.0.1.tar.gz"
   sha256 "9189993c3201dc354a73fdbd5dfdecb8ae077ef06e2d3badc6ac6450e7c64eaa"
@@ -10,13 +10,12 @@ class JfrogCliGo < Formula
     ENV["GOPATH"] = buildpath
     system "go", "get", "github.com/jfrogdev/jfrog-cli-go/..."
     system "go", "build", "-o", "#{bin}/jfrog", "github.com/jfrogdev/jfrog-cli-go/jfrog"
-    
   end
 
   test do
-    actual = pipe_output("#{bin}/jfrog upload")
-    expected = "The --url option is mandatory\n"
+    system "#{bin}/jfrog", "rt", "config", "--interactive=false", "--enc-password=false", "--url=\"http://loaclhost:8081/artifactory\" --user=\"admin\" --password=\"password\""
+    actual = pipe_output("#{bin}/jfrog rt config show")
+    expected = "Url: http://loaclhost:8081/artifactory/\nUser: admin\nPassword: ***\n"
     assert_equal expected, actual
-    system "false"
   end
 end
